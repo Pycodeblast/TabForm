b3f17bdeba73ce5fa791bcffa9b1af22ddf577a1
Pycodeblast <122432833+Pycodeblast@users.noreply.github.com>
2025-04-08 15:26:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TabForm from "./component/TabForm";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1> Multi-Step Form</h1>
+      <TabForm />
+    </div>
+  );
+}
+export default App;
